86e34bfe73ab4d5254a60145a1770d4917a48350
Bruno Souto <bruno.csouto@gmail.com>
2020-06-25 14:44:44 -0300

    feature: facebook authentication

--- App.js
+++ App.js
@@ -1,19 +1,75 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, SafeAreaView, StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { LoginButton, AccessToken, GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    loading: false,
+    user: null
+  }
+
+  getUserCallback = (error, result) => {
+    if (error) {
+      console.log('getUserError', error);
+    } else {
+      this.setState({ user: result, loading: false });
+    }
+  }
+
+  getUserInfo = (token) => {
+    const infoRequest = new GraphRequest(
+      '/me',
+      {
+        accessToken: token,
+        parameters: {
+          fields: { string: 'email, name' }
+        }
+      },
+      this.getUserCallback
+    );
+
+    new GraphRequestManager().addRequest(infoRequest).start();
+  }
+
+  render() {
+    return (
+
+      <SafeAreaView style={styles.container}>
+        <View style={styles.content}>
+          {this.state.loading && <ActivityIndicator />}
+          {this.state.user && <>
+            <Text style={styles.userName}>{this.state.user.name}</Text>
+            <Text style={styles.userEmail}>{this.state.user.email}</Text>
+          </>}
+        </View>
+
+        <LoginButton
+          permissions={['public_profile', 'email']}
+          onLoginFinished={async (error, result) => {
+            if (error) {
+              console.log('auth error', error);
+            } else if (result.isCancelled) {
+              console.log('isCancelled');
+            } else {
+              const { accessToken } = await AccessToken.getCurrentAccessToken();
+
+              this.setState({ loading: true });
+              this.getUserInfo(accessToken);
+            }
+            
+          }}
+          onLogoutFinished={() => {
+            this.setState({user: null});
+          }}
+        />
+      </SafeAreaView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -21,16 +77,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  content: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  userName: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: '#333'
   },
+  userEmail: {
+    fontSize: 14,
+    color: '#666'
+  }
 });
